15eb47031259d8ded1951430b79a1bfc46750ae3
Kelly Sacks <sacks.k.lawrence@aexp.com>
2021-07-22 16:25:44 +0900

    update to tailwind 2.2.6 fix

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const IndexPage = () => (
     <div className="p-4">
-        <h1 className="text-xl text-red-500">Gatsby Tailwind Test</h1>
-        <p>This is some text on the home page</p>
+        <h1 className="text-2xl text-red-500">Gatsby Tailwind Test</h1>
+        <p className="text-lg mt-4">This is some text on the home page for you</p>
     </div>
 );
 
